8753175c0fd1e3be753bf25bdc1722e739ac6276
stefccc <stefan353sr@gmail.com>
2025-06-13 14:32:31 +0200

    base style of techstack, imported svgs for technologies

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,9 +1,18 @@
+import "./TechStack.css";
 const TechStack = () => {
-  const logosUrl: string[] = [];
+  const logosUrl: string[] = [
+    "/csharp.svg",
+    "/php.svg",
+    "/js.svg",
+    "/html.svg",
+    "/css.svg",
+    "/git.svg",
+    "/sql.svg",
+  ];
   return (
-    <div>
+    <div className="techStack">
       <h2>What I commonly use</h2>
-      <div>
+      <div className="tech">
         {logosUrl.map((url) => (
           <img src={url} alt="logo" />
         ))}
